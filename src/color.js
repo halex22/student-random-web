489c516d6a1ff7e62ec40f07d93f14412feae8fe
halex22 <halex2209@gmail.com>
2025-03-12 09:26:32 +0100

    change import ulr

--- src/color.js
+++ src/color.js
@@ -1,4 +1,5 @@
-import UColor from '../node_modules/@acca-alex-22/uber-color/src/index.js'
+// import UColor from '../node_modules/@acca-alex-22/uber-color/src/index.js'
+import UColor from './libs/index.js'
 
 
 const randomColor = UColor.random()
